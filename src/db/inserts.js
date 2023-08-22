6c7f2a78f742be129883f69c03de4a65d429c618
JJMoraless <jjmorales.dev@gmail.com>
2023-08-22 13:21:36 -0500

    feat: :sparkles: agregada funciones apra isnertar datos mas rapido
    se cregaron funciones para convertir inserts de mysql a un array de objetos y añadirlos a mongodb

--- src/db/inserts.js
+++ src/db/inserts.js
@@ -1,132 +1,131 @@
 import db from "./conection.js";
-const bodegas = db.collection("bodegas");
 
-const bodegasDocs = [
-  {
-    id: 11,
-    nombre: "bodega0",
-    id_responsable: 16,
-    estado: 1,
-    created_by: 16,
-    update_by: null,
-    created_at: new Date("2022-06-02T15:33:48"),
-    updated_at: null,
-    deleted_at: null,
-  },
-  {
-    id: 12,
-    nombre: "bodega1",
-    id_responsable: 18,
-    estado: 1,
-    created_by: 18,
-    update_by: null,
-    created_at: new Date("2022-06-02T15:33:48"),
-    updated_at: null,
-    deleted_at: null,
-  },
-  {
-    id: 13,
-    nombre: "bodega2",
-    id_responsable: 18,
-    estado: 1,
-    created_by: 18,
-    update_by: null,
-    created_at: new Date("2022-06-02T15:33:48"),
-    updated_at: null,
-    deleted_at: null,
-  },
+/* -------------------------------------------------------------------------- */
+/*                                   bodegas                                  */
+/* -------------------------------------------------------------------------- */
+import { bodegasData } from "../../data/bodegasData.js";
+import db from "./conection.js";
+const Bodegas = db.collection("bodegas");
+const bodegas = await Bodegas.insertMany(bodegasData());
+
+/* -------------------------------------------------------------------------- */
+/*                                  productos                                 */
+/* -------------------------------------------------------------------------- */
+import { productosData } from "../../data/productosData.js";
+import db from "./conection.js";
+const Productos = db.collection("productos");
+const productos = await Productos.insertMany(productosData());
+
+/* -------------------------------------------------------------------------- */
+/*                                 inventarios                                */
+/* -------------------------------------------------------------------------- */
+import { inventariosData } from "../../data/inventariosData.js";
+import db from "./conection.js";
+const Inventarios = db.collection("inventarios");
+const inventarios = await Inventarios.insertMany(inventariosData());
+
+/* -------------------------------------------------------------------------- */
+/*                                 historiales                                */
+/* -------------------------------------------------------------------------- */
+import db from "./conection.js";
+import { productosData } from "../../data/productosData.js";
+const historialesDocs = [
   {
     id: 18,
-    nombre: "bodega7",
-    id_responsable: 17,
-    estado: 1,
-    created_by: 17,
+    cantidad: 7,
+    id_bodega_origen: 12,
+    id_bodega_destino: 18,
+    id_inventario: 18,
+    created_by: 18,
     update_by: null,
-    created_at: new Date("2022-06-02T15:33:48"),
-    updated_at: null,
-    deleted_at: null,
-  },
-  {
-    id: 19,
-    nombre: "bodega8",
-    id_responsable: 11,
-    estado: 1,
-    created_by: 11,
-    update_by: null,
-    created_at: new Date("2022-06-02T15:33:48"),
+    created_at: null,
     updated_at: null,
     deleted_at: null,
   },
   {
     id: 20,
-    nombre: "bodega9",
-    id_responsable: 18,
-    estado: 1,
-    created_by: 18,
+    cantidad: 9,
+    id_bodega_origen: 11,
+    id_bodega_destino: 12,
+    id_inventario: 18,
+    created_by: 15,
     update_by: null,
-    created_at: new Date("2022-06-02T15:33:48"),
+    created_at: null,
     updated_at: null,
     deleted_at: null,
   },
   {
-    id: 22,
-    nombre: "jsjsjss",
-    id_responsable: 13,
-    estado: 1,
+    id: 21,
+    cantidad: 7,
+    id_bodega_origen: 29,
+    id_bodega_destino: 30,
+    id_inventario: 36,
     created_by: null,
     update_by: null,
-    created_at: new Date("2022-06-02T21:30:47"),
-    updated_at: new Date("2022-06-02T21:30:47"),
+    created_at: new Date("2022-06-03T03:18:19"),
+    updated_at: new Date("2022-06-03T03:18:19"),
+    deleted_at: null,
+  },
+  {
+    id: 22,
+    cantidad: 666,
+    id_bodega_origen: 26,
+    id_bodega_destino: 24,
+    id_inventario: 27,
+    created_by: null,
+    update_by: null,
+    created_at: new Date("2022-06-03T03:19:14"),
+    updated_at: new Date("2022-06-03T03:19:14"),
     deleted_at: null,
   },
   {
     id: 23,
-    nombre: "leeche",
-    id_responsable: 13,
-    estado: 1,
+    cantidad: 100,
+    id_bodega_origen: 19,
+    id_bodega_destino: 18,
+    id_inventario: 41,
     created_by: null,
     update_by: null,
-    created_at: new Date("2022-06-02T21:43:08"),
-    updated_at: new Date("2022-06-02T21:43:08"),
+    created_at: new Date("2022-06-03T03:20:24"),
+    updated_at: new Date("2022-06-03T03:20:24"),
     deleted_at: null,
   },
   {
     id: 24,
-    nombre: "soto1",
-    id_responsable: 15,
-    estado: 1,
+    cantidad: 25,
+    id_bodega_origen: 19,
+    id_bodega_destino: 26,
+    id_inventario: 41,
     created_by: null,
     update_by: null,
-    created_at: new Date("2022-06-02T21:46:40"),
-    updated_at: new Date("2022-06-02T21:46:40"),
+    created_at: new Date("2022-06-03T03:27:47"),
+    updated_at: new Date("2022-06-03T03:27:47"),
     deleted_at: null,
   },
   {
     id: 25,
-    nombre: "soto2",
-    id_responsable: 15,
-    estado: 1,
+    cantidad: 25,
+    id_bodega_origen: 19,
+    id_bodega_destino: 26,
+    id_inventario: 41,
     created_by: null,
     update_by: null,
-    created_at: new Date("2022-06-02T21:47:29"),
-    updated_at: new Date("2022-06-02T21:47:29"),
+    created_at: new Date("2022-06-03T03:28:27"),
+    updated_at: new Date("2022-06-03T03:28:27"),
     deleted_at: null,
   },
   {
     id: 26,
-    nombre: "prueba",
-    id_responsable: 12,
-    estado: 1,
+    cantidad: 300,
+    id_bodega_origen: 18,
+    id_bodega_destino: 12,
+    id_inventario: 42,
     created_by: null,
     update_by: null,
-    created_at: new Date("2022-06-02T21:47:48"),
-    updated_at: new Date("2022-06-02T21:47:48"),
+    created_at: new Date("2022-06-03T03:51:23"),
+    updated_at: new Date("2022-06-03T03:51:23"),
     deleted_at: null,
   },
 ];
-
-try {
-  await bodegas.insertMany(bodegasDocs);
-} catch (error) {
-  console.log("🚀 ~ file: inserts.js:131 ~ error:", error);
-}
+const Historiales = db.collection("historiales");
+const historiales = await Historiales.insertMany(historialesDocs);
--- src/db/shemas.js
+++ src/db/shemas.js
@@ -1,5 +1,60 @@
 import db from "./conection.js";
 
+/* -------------------------------------------------------------------------- */
+/*                                   bodegas                                  */
+/* -------------------------------------------------------------------------- */
+import db from "./conection.js";
+await db.createCollection("bodegas", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      required: ["nombre", "id_responsable", "estado"],
+      properties: {
+        id: {
+          bsonType: "int",
+          description: "El ID de la bodega",
+        },
+        nombre: {
+          bsonType: ["string", "null"],
+          description: "El nombre de la bodega",
+        },
+        id_responsable: {
+          bsonType: ["int", "null"],
+          description: "ID del responsable de la bodega",
+        },
+        estado: {
+          bsonType: ["int", "null"],
+          description: "El estado de la bodega",
+        },
+        created_by: {
+          bsonType: ["int", "null"],
+          description: "ID del creador de la bodega",
+        },
+        updated_by: {
+          bsonType: ["int", "null"],
+          description: "ID del usuario que actualizó la bodega",
+        },
+        created_at: {
+          bsonType: ["date", "null", "string"],
+          description: "Fecha de creación de la bodega",
+        },
+        updated_at: {
+          bsonType: ["date", "null", "string"],
+          description: "Fecha de actualización de la bodega",
+        },
+        deleted_at: {
+          bsonType: ["date", "null", "string"],
+          description: "Fecha de eliminación de la bodega",
+        },
+      },
+    },
+  },
+});
+
+/* -------------------------------------------------------------------------- */
+/*                                  productos                                 */
+/* -------------------------------------------------------------------------- */
+import db from "./conection.js";
 await db.createCollection("productos", {
   validator: {
     $jsonSchema: {
@@ -7,11 +62,11 @@ await db.createCollection("productos", {
       required: ["nombre", "descripcion", "estado", "created_by"],
       properties: {
         id: {
-          bsonType: ["long", "null"],
+          bsonType: ["int"],
           description: "El ID del producto",
         },
         nombre: {
-          bsonType: ["string", "null"],
+          bsonType: ["string"],
           description: "El nombre del producto",
         },
         descripcion: {
@@ -23,7 +78,7 @@ await db.createCollection("productos", {
           description: "El estado del producto",
         },
         created_by: {
-          bsonType: ["long", "null"],
+          bsonType: ["int", "null"],
           description: "ID del creador del producto",
         },
         updated_by: {
@@ -31,15 +86,15 @@ await db.createCollection("productos", {
           description: "ID del usuario que actualizó el producto",
         },
         created_at: {
-          bsonType: ["date", "null"],
+          bsonType: ["date", "null", "string"],
           description: "Fecha de creación del producto",
         },
         updated_at: {
-          bsonType: ["date", "null"],
+          bsonType: ["date", "null", "string"],
           description: "Fecha de actualización del producto",
         },
         deleted_at: {
-          bsonType: ["date", "null"],
+          bsonType: ["date", "null", "string"],
           description: "Fecha de eliminación del producto",
         },
       },
@@ -47,6 +102,11 @@ await db.createCollection("productos", {
   },
 });
 
+/* -------------------------------------------------------------------------- */
+/*                                 historiales                                */
+/* -------------------------------------------------------------------------- */
+
+import db from "./conection.js";
 await db.createCollection("historiales", {
   validator: {
     $jsonSchema: {
@@ -54,35 +114,35 @@ await db.createCollection("historiales", {
       required: ["cantidad", "created_by"],
       properties: {
         id: {
-          bsonType: "long",
+          bsonType: ["int", "null"],
           description: "El ID del historial",
         },
         cantidad: {
-          bsonType: "int",
+          bsonType: ["int", "null"],
           description: "La cantidad en el historial",
         },
         id_bodega_origen: {
-          bsonType: ["long", "null"],
+          bsonType: ["int", "null"],
           description: "ID de la bodega de origen",
         },
         id_bodega_destino: {
-          bsonType: ["long", "null"],
+          bsonType: ["int", "null"],
           description: "ID de la bodega de destino",
         },
         id_inventario: {
-          bsonType: ["long", "null"],
+          bsonType: ["int", "null"],
           description: "ID del inventario relacionado",
         },
         created_by: {
-          bsonType: "long",
+          bsonType: ["int", "null"],
           description: "ID del creador del historial",
         },
         updated_by: {
-          bsonType: ["long", "null"],
+          bsonType: ["int", "null"],
           description: "ID del usuario que actualizó el historial",
         },
         created_at: {
-          bsonType: "date",
+          bsonType: ["date", "null"],
           description: "Fecha de creación del historial",
         },
         updated_at: {
@@ -98,6 +158,11 @@ await db.createCollection("historiales", {
   },
 });
 
+/* -------------------------------------------------------------------------- */
+/*                                 inventarios                                */
+/* -------------------------------------------------------------------------- */
+
+import db from "./conection.js";
 await db.createCollection("inventarios", {
   validator: {
     $jsonSchema: {
@@ -105,39 +170,39 @@ await db.createCollection("inventarios", {
       required: ["id_bodega", "id_producto", "cantidad", "created_by"],
       properties: {
         id: {
-          bsonType: "long",
+          bsonType: "int",
           description: "El ID del inventario",
         },
         id_bodega: {
-          bsonType: "long",
+          bsonType: ["int", "null"],
           description: "ID de la bodega relacionada",
         },
         id_producto: {
-          bsonType: "long",
+          bsonType: ["int", "null"],
           description: "ID del producto relacionado",
         },
         cantidad: {
-          bsonType: "int",
+          bsonType: ["int", "null"],
           description: "La cantidad en el inventario",
         },
         created_by: {
-          bsonType: "long",
+          bsonType: ["int", "null"],
           description: "ID del creador del inventario",
         },
         updated_by: {
-          bsonType: ["long", "null"],
+          bsonType: ["int", "null"],
           description: "ID del usuario que actualizó el inventario",
         },
         created_at: {
-          bsonType: "date",
+          bsonType: ["date", "null", "string"],
           description: "Fecha de creación del inventario",
         },
         updated_at: {
-          bsonType: ["date", "null"],
+          bsonType: ["date", "null", "string"],
           description: "Fecha de actualización del inventario",
         },
         deleted_at: {
-          bsonType: ["date", "null"],
+          bsonType: ["date", "null", "string"],
           description: "Fecha de eliminación del inventario",
         },
       },
@@ -145,53 +210,10 @@ await db.createCollection("inventarios", {
   },
 });
 
-await db.createCollection("bodegas", {
-  validator: {
-    $jsonSchema: {
-      bsonType: "object",
-      required: ["nombre", "id_responsable", "estado", "created_by"],
-      properties: {
-        id: {
-          bsonType: ["long", "null"],
-          description: "El ID de la bodega",
-        },
-        nombre: {
-          bsonType: ["string", "null"],
-          description: "El nombre de la bodega",
-        },
-        id_responsable: {
-          bsonType: ["long", "null"],
-          description: "ID del responsable de la bodega",
-        },
-        estado: {
-          bsonType: ["int", "null"],
-          description: "El estado de la bodega",
-        },
-        created_by: {
-          bsonType: ["long", "null"],
-          description: "ID del creador de la bodega",
-        },
-        updated_by: {
-          bsonType: ["long", "null"],
-          description: "ID del usuario que actualizó la bodega",
-        },
-        created_at: {
-          bsonType: ["date", "null"],
-          description: "Fecha de creación de la bodega",
-        },
-        updated_at: {
-          bsonType: ["date", "null"],
-          description: "Fecha de actualización de la bodega",
-        },
-        deleted_at: {
-          bsonType: ["date", "null"],
-          description: "Fecha de eliminación de la bodega",
-        },
-      },
-    },
-  },
-});
-
+/* -------------------------------------------------------------------------- */
+/*                                    users                                   */
+/* -------------------------------------------------------------------------- */
+import db from "./conection.js";
 await db.createCollection("users", {
   validator: {
     $jsonSchema: {
@@ -213,7 +235,7 @@ await db.createCollection("users", {
           uniqueItems: true,
         },
         email_verified_at: {
-          bsonType: ["date", "null"],
+          bsonType: "date",
           description: "Fecha de verificación del email",
         },
         estado: {
@@ -225,7 +247,7 @@ await db.createCollection("users", {
           description: "ID del creador del usuario",
         },
         updated_by: {
-          bsonType: ["long", "null"],
+          bsonType: "long",
           description: "ID del usuario que actualizó los datos",
         },
         foto: {
@@ -241,11 +263,11 @@ await db.createCollection("users", {
           description: "Fecha de creación del usuario",
         },
         updated_at: {
-          bsonType: ["date", "null"],
+          bsonType: "date",
           description: "Fecha de actualización del usuario",
         },
         deleted_at: {
-          bsonType: ["date", "null"],
+          bsonType: "date",
           description: "Fecha de eliminación del usuario",
         },
       },
